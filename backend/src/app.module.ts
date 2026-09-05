e49a775594f07ae023fd88d2f1e3fa82cc5eb9ac
nroadcompany-ux <nroadcompany@gmail.com>
2026-09-05 21:39:25 +0900

    feat: register Sprint 1 core mock controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
 import { HealthController } from './health.controller'
+import { CoreMockController } from './core-mock.controller'
 
 @Module({
-  controllers: [HealthController],
+  controllers: [HealthController, CoreMockController],
 })
 export class AppModule {}
